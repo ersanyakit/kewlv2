2925cbd1014e13af91057ef297c3c209df91e261
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2025-05-12 05:43:55 +0700

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TradeTerminal from './components/Swap/TradeTerminal';
 import Layout from './components/Layout/Layout';
+import About from './components/About/About';
 import { TokenProvider } from './context/TokenContext';
 import { Web3ProviderContext } from './context/Web3ProviderContext';
 import { SwapProvider } from './context/SwapContext';
@@ -8,13 +10,19 @@ import { SwapProvider } from './context/SwapContext';
 function App() {
   return (
     <Web3ProviderContext>
-    <TokenProvider>
-      <SwapProvider>
-      <Layout>
-        <TradeTerminal />
-      </Layout>
-      </SwapProvider>
-    </TokenProvider>
+      <TokenProvider>
+        <SwapProvider>
+          <Router>
+            <Layout>
+              <Routes>
+                <Route path="/" element={<TradeTerminal />} />
+                <Route path="/swap" element={<TradeTerminal />} />
+                <Route path="/about" element={<About />} />
+              </Routes>
+            </Layout>
+          </Router>
+        </SwapProvider>
+      </TokenProvider>
     </Web3ProviderContext>
   );
 }
---
+++ src/components/About/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <h1>Hakkımızda</h1>
+      <p>Bu uygulama hakkında bilgiler buraya gelecek.</p>
+    </div>
+  );
+};
+
+export default About; 
