821525caa24e019b8042fc887d4b7499bd86cb7a
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2025-07-01 12:51:18 +0700

    safe area fixes

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -31,7 +31,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 
   const location = useLocation();
   return (
-    <div className={`${isDarkMode ? 'bg-gray-900 text-gray-100' : 'bg-gradient-to-br from-gray-50 via-gray-100 to-gray-200 text-gray-900'} min-h-screen  flex flex-col gap-2 p-4 md:p-2 scrollbar-hide transition-colors duration-300`}>
+    <div className={`${isDarkMode ? 'bg-gray-900 text-gray-100' : 'bg-gradient-to-br from-gray-50 via-gray-100 to-gray-200 text-gray-900'} min-h-screen safe-padding bg-white min-h-screen flex flex-col gap-2 p-4 md:p-2 scrollbar-hide transition-colors duration-300`}>
       <Navbar />
      
       
--- tailwind.config.js
+++ tailwind.config.js
@@ -128,6 +128,12 @@ module.exports = {
         'height': 'height',
         'spacing': 'margin, padding',
       },
+      padding: {
+        'safe-top': 'env(safe-area-inset-top)',
+        'safe-bottom': 'env(safe-area-inset-bottom)',
+        'safe-left': 'env(safe-area-inset-left)',
+        'safe-right': 'env(safe-area-inset-right)',
+      },
     },
   },
   plugins: [
