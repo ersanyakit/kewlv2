859dfd09c450d75805ebf86e728f712473a81d51
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2025-07-07 00:21:33 +0700

    fixes

--- src/context/Web3ProviderContext.tsx
+++ src/context/Web3ProviderContext.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { EthersAdapter } from '@reown/appkit-adapter-ethers';
-import { chiliz as originalChiliz,spicy,  avalanche,base, arbitrum, sonic,hardhat, Chain, AppKitNetwork } from '@reown/appkit/networks';
+import { chiliz as originalChiliz,spicy, bsc, avalanche,base, arbitrum, sonic,hardhat, Chain, AppKitNetwork } from '@reown/appkit/networks';
 import { AppKitOptions, createAppKit, useAppKit } from '@reown/appkit/react';
 import { createContext, ReactNode, useContext } from 'react';
 import { createPublicClient, createWalletClient, http } from 'viem';
@@ -65,7 +65,8 @@ export function getChainName(chainId: number): string {
     146: "sonic",
     8453:"base",
     31337:"hardhat",
-    1907:"bitci"
+    1907:"bitci",
+    56:"bsc"
   };
   return chains[chainId] || chains[DEFAULT_CHAIN_ID];
 }
@@ -86,7 +87,7 @@ const metadata = {
 export const appkitOptions: AppKitOptions = {
   adapters: [new EthersAdapter()],
   metadata: metadata,
-  networks: [chiliz,base,sonic, avalanche, bitci,arbitrum,hardhat,spicy],
+  networks: [chiliz,bsc,base,sonic, avalanche, bitci,arbitrum,hardhat,spicy],
   defaultNetwork: chiliz,
   chainImages: {
     88888: "/assets/chains/chz.svg",
@@ -94,8 +95,8 @@ export const appkitOptions: AppKitOptions = {
     146: "/assets/chains/sonic.svg",
     8453:"/assets/chains/base.svg",
     31337:"/assets/chains/error.svg",
-    1907:"/assets/chains/bitci.svg"
-    
+    1907:"/assets/chains/bitci.svg",
+    56:"/assets/chains/bsc.svg"
   },
   projectId,
   themeMode: "light",
@@ -112,13 +113,13 @@ export const appkitOptions: AppKitOptions = {
  
   featuredWalletIds: [
     'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // Metamask
-    '56843177b5e89d4bcb19a27dab7c49e0f33d8d3a6c8c4c7e5274f605e92befd6',//socios
     'f323633c1f67055a45aac84e321af6ffe46322da677ffdd32f9bc1e33bafe29c',
     '971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709',
     '8a0ee50d1f22f6651afcae7eb4253e52a3310b90af5daef78a8c4929a9bb99d4',
     '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
     '971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709',
     'e7c4d26541a7fd84dbdfa9922d3ad21e936e13a7a0e44385d44f006139e44d3b', // WalletConnect
+    '56843177b5e89d4bcb19a27dab7c49e0f33d8d3a6c8c4c7e5274f605e92befd6',//socios
   ],
 }
 
